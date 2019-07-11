fd8d805dde60aca5c3bc4d490a9e6e0becafb0ce
Chris <50994413+chrise01@users.noreply.github.com>
2019-07-11 16:09:32 +1000

    Create k8.sh

---
+++ k8.sh
@@ -0,0 +1,15 @@
+
+sudo apt install docker.io -y
+sudo systemctl enable docker
+sudo apt install curl -y
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt install kubeadm
+kubeadm version
+#this turns off swapping in memory
+sudo swapoff -a
+#hostname - need to change for each node
+sudo hostnamectl set-hostname master-node
+#hostnamectl set-hostname slave-node
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
