adccc95d310bd531eca8b035a58284f23ad0b55b
Chris Evans <chrise@vmware.com>
2019-07-12 11:51:18 +0930

    Update to include slave options

--- k8-Slave.sh
+++ k8-Slave.sh
@@ -1,1 +1,18 @@
+sudo apt-get update -y && sudo apt-get upgrade -y
+#leave this off unless you want an OS upgrade
+#sudo apt-get upgrade -y
+sudo apt install docker.io -y
+sudo systemctl enable docker
+sudo apt install curl -y
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt install kubeadm
+kubeadm version
+#this turns off swapping in memory
 sudo swapoff -a
+hostnamectl set-hostname slave-node
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+#What next?
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
