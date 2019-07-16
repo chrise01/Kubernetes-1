374e0aea6811eeace2f4a5d7101309ee99201fee
Chris Evans <chrise@vmware.com>
2019-07-16 09:47:03 +0930

    Removed busted stuff

--- k8.sh
+++ k8.sh
@@ -7,14 +7,18 @@ sudo apt install curl -y
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt install kubeadm
-kubeadm version
+#kubeadm version
 #this turns off swapping in memory
 sudo swapoff -a
 #hostname - need to change for each node
 sudo hostnamectl set-hostname master-node
 #hostnamectl set-hostname slave-node
+##Use this to start it
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 #What next?
-#mkdir -p $HOME/.kube
-#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-#sudo chown $(id -u):$(id -g) $HOME/.kube/config
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+#Dashboard and proxy
+sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta1/aio/deploy/recommended.yaml
+sudo kubectl proxy
