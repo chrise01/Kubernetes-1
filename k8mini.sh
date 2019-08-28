87810d411d69c19f1b3fa1e8128ccb797af42cf4
Chris <50994413+chrise01@users.noreply.github.com>
2019-08-28 16:25:55 +0930

    Update k8mini.sh

--- k8mini.sh
+++ k8mini.sh
@@ -5,6 +5,7 @@
 sudo apt-get update
 sudo apt-get install -y apt-transport-https
 ##
+sudo apt-get install -y virtualbox virtualbox-ext-pack
 #curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 #sudo touch /etc/apt/sources.list.d/kubernetes.list 
 #echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
