dd327865eaf12b215611444bc3e335baeecf90b8
Chris <50994413+chrise01@users.noreply.github.com>
2019-08-28 15:42:06 +0930

    Update k8mini.sh

--- k8mini.sh
+++ k8mini.sh
@@ -14,3 +14,4 @@ sudo apt-get install -y kubectl
 ##
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.2/minikube-linux-amd64
 chmod +x minikube && sudo mv minikube /usr/local/bin/
+
