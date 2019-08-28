fdc693b8af46f29b78be7fe0fac501dd297a7012
Chris <50994413+chrise01@users.noreply.github.com>
2019-08-28 16:09:49 +0930

    Update k8mini.sh

--- k8mini.sh
+++ k8mini.sh
@@ -5,18 +5,19 @@
 sudo apt-get update
 sudo apt-get install -y apt-transport-https
 ##
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo touch /etc/apt/sources.list.d/kubernetes.list 
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
+#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+#sudo touch /etc/apt/sources.list.d/kubernetes.list 
+#echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+#sudo apt-get update
+sudo snap install kubectl --classic
+
 
 ##sudo apt-get install -y kubectl - no longer works?
 sudo snap install minikube
 ##
-wget -N minikube https://storage.googleapis.com/minikube/releases/v0.28.2/minikube-linux-amd64
-chmod +x minikube && sudo mv minikube /usr/local/bin/
+#wget -N minikube https://storage.googleapis.com/minikube/releases/v0.28.2/minikube-linux-amd64
+#chmod +x minikube && sudo mv minikube /usr/local/bin/
 ##
- minikube start
+minikube start
 kubectl api-versions
 
