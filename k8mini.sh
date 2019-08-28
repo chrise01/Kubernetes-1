3bab167daf98235b592663096d40c408cacd65b7
Chris <50994413+chrise01@users.noreply.github.com>
2019-08-28 15:47:41 +0930

    Update k8mini.sh

--- k8mini.sh
+++ k8mini.sh
@@ -1,17 +1,16 @@
-## wget -N https://raw.githubusercontent.com/chrise01/Kubernetes-1/master/k8mini.sh
-##chmod -x ./k8mini.sh
-#maybe update and snap 1st.
-bash ./k8mini.sh
+wget -N https://raw.githubusercontent.com/chrise01/Kubernetes-1/master/k8mini.sh
+chmod -x ./k8mini.sh
+sudo bash ./k8mini.sh
 ##
 sudo apt-get update
 sudo apt-get install -y apt-transport-https
 ##
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+wget -N https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo touch /etc/apt/sources.list.d/kubernetes.list 
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
 ##
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.2/minikube-linux-amd64
+wget -N minikube https://storage.googleapis.com/minikube/releases/v0.28.2/minikube-linux-amd64
 chmod +x minikube && sudo mv minikube /usr/local/bin/
 
