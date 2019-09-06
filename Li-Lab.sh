8957d90cfee9c2bd4380bea8252a97dc3d17f1bd
Chris <50994413+chrise01@users.noreply.github.com>
2019-09-06 13:31:14 +0930

    Update Li-Lab.sh

--- Li-Lab.sh
+++ Li-Lab.sh
@@ -1,7 +1,8 @@
 sudo snap install docker
 sudo docker version
+sudo apt-get install curl -y
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 
 sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-kubectl version
+sudo kubectl version
