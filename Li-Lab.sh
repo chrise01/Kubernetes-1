5338cd1202efb0c5bb243808d767b2f0b7bb92a8
Chris <50994413+chrise01@users.noreply.github.com>
2019-09-06 13:39:50 +0930

    Update Li-Lab.sh

--- Li-Lab.sh
+++ Li-Lab.sh
@@ -6,3 +6,9 @@ sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s
 sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 sudo kubectl version
+sudo apt install virtualbox -y
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \ && chmod +x minikube
+sudo install minikube /usr/local/bin
+minikube version
+minikube start
+
